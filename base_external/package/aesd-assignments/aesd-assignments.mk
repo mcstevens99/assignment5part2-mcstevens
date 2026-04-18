6bd2870fafaf7bebc2c0e168844b8a621f594bc2
mcstevens99 <cesteves@alumni.uminho.pt>
2026-04-18 23:06:26 +0100

    files modified for assignmet 5 part 2

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -5,33 +5,26 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = fca5198a9233867d9b6d9b16f889a8a603fdb0b8
+AESD_ASSIGNMENTS_VERSION = 2450f95cfd3298d05ac4307827ecdac5b1beebc8
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = git@github.com:mcstevens99/assignment3-mcstevens.git
+AESD_ASSIGNMENTS_SITE = git@github.com:mcstevens99/assignment4-mcstevens.git
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 #$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/writer-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/server all
 endef
 
-# TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
-
 	$(INSTALL) -d $(TARGET_DIR)/usr/bin
-	$(INSTALL) -d $(TARGET_DIR)/etc/finder-app/conf
+	$(INSTALL) -d $(TARGET_DIR)/etc/init.d
 
-	$(INSTALL) -m 0755 $(@D)/writer-app/writer $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder.sh $(TARGET_DIR)/usr/bin/
-	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/usr/bin/
-	
-	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
-	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -m 0755 $(@D)/server/aesdsocket-start-stop.sh $(TARGET_DIR)/etc/init.d/S99aesdsocket
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4-buildroot/* $(TARGET_DIR)/usr/bin
-
 endef
 
 $(eval $(generic-package))
